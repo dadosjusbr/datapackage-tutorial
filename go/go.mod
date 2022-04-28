1ca430b4d06b4b534e17a429e46138aab3dd997d
Daniel Fireman <danielfireman@gmail.com>
2022-04-28 15:37:54 +0000

    first version of go tutorial

---
+++ go/go.mod
@@ -0,0 +1,15 @@
+module datapackage-tutorial
+
+go 1.18
+
+require (
+	github.com/frictionlessdata/datapackage-go v1.0.2
+	github.com/frictionlessdata/tableschema-go v1.1.4-0.20220401172006-6cc5f3b2411c
+	gonum.org/v1/gonum v0.11.0
+)
+
+require (
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+)
---
+++ go/tutorial.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/frictionlessdata/datapackage-go/datapackage"
+	"github.com/frictionlessdata/tableschema-go/csv"
+	"gonum.org/v1/gonum/stat"
+)
+
+const (
+	pkgURL = "tjal-2021-12.zip"
+)
+
+func main() {
+	pkg, err := datapackage.Load(pkgURL)
+	if err != nil {
+		log.Fatalf("Error loading datapackage (%s):%v", pkgURL, err)
+	}
+
+	var remunerations []float64
+	err = pkg.GetResource("remuneracao").CastColumn("valor", &remunerations, csv.LoadHeaders())
+	if err != nil {
+		log.Fatalf("Error casting datapackage (%s):%v", pkgURL, err)
+	}
+
+	fmt.Println(stat.MeanStdDev(remunerations, nil))
+}
