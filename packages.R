7f466f853a1625376a2beac96ec2251a8bdbfe99
Raul de Sá Durlo <durlo.raul@gmail.com>
2022-05-09 21:05:15 +0000

    add packages

--- packages.R
+++ packages.R
@@ -1,1 +1,4 @@
+install.packages("jsonlite", repo = "https://cloud.r-project.org/")
 install.packages("tidyverse", repo = "https://cloud.r-project.org/")
+install.packages("devtools", repo = "https://cloud.r-project.org/")
+devtools::install_github("frictionlessdata/datapackage-r")
