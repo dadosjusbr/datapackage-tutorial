7e2cd0a0219a37645fc2496c10b1aa045b171827
Raul de Sá Durlo <durlo.raul@gmail.com>
2022-05-10 00:30:55 +0000

    tutorial R

--- R/tutorial.R
+++ R/tutorial.R
@@ -1,12 +1,24 @@
-install.packages("jsonlite")
-install.packages("tidyverse")
-install.packages("devtools")
-devtools::install_github("frictionlessdata/datapackage-r")
-print("hello world")
-
 library(jsonlite)
 library(tidyverse)
-library(datapackage-r)
+library(datapackage.r)
+library(tableschema.r)
+library(here)
 
+install.packages("frictionless")
+
+# URL package
 pkgURL <- "https://dadosjusbr.org/download/datapackage/tjal/tjal-2021-12.zip"
 
+# Download file
+download.file(pkgURL, mode = "wb", destfile = here("R/data/tjal-2021-12.zip"))
+
+# Unzip file
+unzip(here("R/data/tjal-2021-12.zip"), exdir = here::here("R/data"))
+
+dataPackage = Package.load(here('R/data/datapackage.json'))
+dataPackage = dataPackage$infer(here('R/data/*.csv'))
+dataPackage$descriptor
+dataPackage$resources
+dataPackage$valid
+remun = dataPackage$getResource('remuneracao')
+remun$table$read()
